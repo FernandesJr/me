391b2b092e543a515d7ac00fe11c425812e17dc7
Fernandes Júnior <fernaandes.jr@gmail.com>
2024-02-07 15:41:03 -0300

    feat: education componet create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { AcademicEducationComponent } from './components/academic-education/academic-education.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'about' , pathMatch: 'full'},
   {path: 'about', component: AboutComponent},
+  {path: 'education', component: AcademicEducationComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { AboutComponent } from './components/about/about.component';
+import { AcademicEducationComponent } from './components/academic-education/academic-education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    AcademicEducationComponent
   ],
   imports: [
     BrowserModule,
